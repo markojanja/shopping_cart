468e95f2295c5d4cacad4893641120121ab0af42
Marko Janjic <marko.m.janjic@gmail.com>
2024-03-23 10:46:36 +0100

    contact page mobile view

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -4,8 +4,8 @@ import { IoLogoFacebook, IoLogoInstagram, IoLogoTwitter } from "react-icons/io5"
 const Contact = () => {
   return (
     <main className='min-h-screen text-black flex items-center justify-center bg-red-400'>
-      <div className='w-4/6 flex bg-slate-50'>
-        <div className='flex flex-col items-center justify-center gap-4 w-1/2 flex-1'>
+      <div className='flex-col md:flex-row md:w-5/6 lg:w-4/6 flex bg-slate-50 pt-24 md:pt-0'>
+        <div className='flex flex-col items-center justify-center gap-4 lg:w-1/2 flex-1'>
           <div className='w-5/6 mx-auto'>
             <h2 className='text-3xl text-red-500 font-extrabold'>Contact Us</h2>
             <p className='text-gray-600'>Get in touch and let us know how we can help</p>
@@ -15,13 +15,13 @@ const Contact = () => {
             <textarea className='border-2 border-gray-500 rounded-sm p-2' name='' id='' cols='30' rows='10'></textarea>
             <button className=' w-[150px] bg-red-500 py-3 px-3 text-white'>Send message</button>
           </form>
-          <div className='flex flex-row w-5/6 items-start justify-start gap-3'>
+          <div className='flex flex-row w-5/6 items-start justify-start gap-3 p-3'>
             <IoLogoFacebook className='text-2xl text-red-500' />
             <IoLogoInstagram className='text-2xl text-red-500' />
             <IoLogoTwitter className='text-2xl text-red-500' />
           </div>
         </div>
-        <div className='relative w-1/2 bg-red-200'>
+        <div className='relative w-full lg:w-1/2 bg-red-200'>
           <img className='h-full w-full' src={email} alt='' />
         </div>
       </div>
